2324aa3e8e3a0c9f9e14837dc99e5c94876ab685
Dirk Schut <dirk@mrdls.com>
2014-09-12 17:24:36 +0200

    Added buttons to the main menu

--- Empires/forms/MainMenu.Designer.cs
+++ Empires/forms/MainMenu.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_NewGame = new System.Windows.Forms.Button();
+            this.btn_LoadGame = new System.Windows.Forms.Button();
+            this.btn_ManagePackages = new System.Windows.Forms.Button();
+            this.btn_Settings = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_NewGame
+            // 
+            this.btn_NewGame.Location = new System.Drawing.Point(13, 13);
+            this.btn_NewGame.Name = "btn_NewGame";
+            this.btn_NewGame.Size = new System.Drawing.Size(259, 23);
+            this.btn_NewGame.TabIndex = 0;
+            this.btn_NewGame.Text = "New Game";
+            this.btn_NewGame.UseVisualStyleBackColor = true;
+            // 
+            // btn_LoadGame
+            // 
+            this.btn_LoadGame.Location = new System.Drawing.Point(13, 43);
+            this.btn_LoadGame.Name = "btn_LoadGame";
+            this.btn_LoadGame.Size = new System.Drawing.Size(259, 23);
+            this.btn_LoadGame.TabIndex = 1;
+            this.btn_LoadGame.Text = "Load Game";
+            this.btn_LoadGame.UseVisualStyleBackColor = true;
+            // 
+            // btn_ManagePackages
+            // 
+            this.btn_ManagePackages.Location = new System.Drawing.Point(13, 73);
+            this.btn_ManagePackages.Name = "btn_ManagePackages";
+            this.btn_ManagePackages.Size = new System.Drawing.Size(259, 23);
+            this.btn_ManagePackages.TabIndex = 2;
+            this.btn_ManagePackages.Text = "Package Manager";
+            this.btn_ManagePackages.UseVisualStyleBackColor = true;
+            // 
+            // btn_Settings
+            // 
+            this.btn_Settings.Location = new System.Drawing.Point(12, 102);
+            this.btn_Settings.Name = "btn_Settings";
+            this.btn_Settings.Size = new System.Drawing.Size(260, 23);
+            this.btn_Settings.TabIndex = 3;
+            this.btn_Settings.Text = "Settings";
+            this.btn_Settings.UseVisualStyleBackColor = true;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(12, 132);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(260, 23);
+            this.btn_Exit.TabIndex = 4;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Settings);
+            this.Controls.Add(this.btn_ManagePackages);
+            this.Controls.Add(this.btn_LoadGame);
+            this.Controls.Add(this.btn_NewGame);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainMenu";
@@ -45,5 +100,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_NewGame;
+        private System.Windows.Forms.Button btn_LoadGame;
+        private System.Windows.Forms.Button btn_ManagePackages;
+        private System.Windows.Forms.Button btn_Settings;
+        private System.Windows.Forms.Button btn_Exit;
     }
 }
