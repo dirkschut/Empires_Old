727a7c741b2d8dcd606c73bfca1529ef24b81e7f
Dirk Schut <dirk@mrdls.com>
2014-09-12 16:43:28 +0200

    Renamed Form1 to MainMenu and put it in a new forms folder

--- Empires/Program.cs
+++ Empires/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
+using Empires.forms;
 namespace Empires
 {
     static class Program
@@ -15,7 +16,7 @@ namespace Empires
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new forms.MainMenu());
         }
     }
 }
