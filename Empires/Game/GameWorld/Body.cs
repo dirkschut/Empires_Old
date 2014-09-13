5927fce081c2dcb403eaa90d57e33ba876d768dd
Dirk Schut <dirk@mrdls.com>
2014-09-13 23:51:13 +0200

    Some data in bodies

--- Empires/Game/GameWorld/Body.cs
+++ Empires/Game/GameWorld/Body.cs
@@ -19,12 +19,41 @@ namespace Empires.Game.GameWorld
         public int solarSystem;
         public int ID;
         public int type;
+        public int size;
+        public int distanceFromCentre;
+
+        public Double position;
+        public Double speed;
 
         public Body(Boolean generate, int solarSystem, int type)
         {
             this.ID = Objects.game.data.bodyCounter;
             Objects.game.data.bodyCounter++;
             this.type = type;
+
+            Random rand = new Random();
+
+            switch (type)
+            {
+                case TYPE_STAR:
+                    distanceFromCentre = 0;
+                    position = 0;
+                    speed = 0;
+                    size = rand.Next(10000, 100000);
+                    break;
+                case TYPE_PLANET:
+                    distanceFromCentre = rand.Next(100, 10000);
+                    position = rand.NextDouble() * 360;
+                    speed = rand.NextDouble();
+                    size = rand.Next(100, 10000);
+                    break;
+                case TYPE_ASTROID:
+                    distanceFromCentre = rand.Next(100, 10000);
+                    position = rand.NextDouble() * 360;
+                    speed = rand.NextDouble();
+                    size = rand.Next(1, 100);
+                    break;
+            }
         }
     }
 }
