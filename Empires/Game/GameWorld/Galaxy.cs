e47e8525a55549373ea67fd72b7b07b49d97f8bc
Dirk Schut <dirk@mrdls.com>
2014-09-15 01:44:26 +0200

    I, the god of this multiverse, have decided that it will be flat-ish

--- Empires/Game/GameWorld/Galaxy.cs
+++ Empires/Game/GameWorld/Galaxy.cs
@@ -12,11 +12,9 @@ namespace Empires.Game.GameWorld
 
         public Double x;
         public Double y;
-        public Double z;
 
         public Double xSpeed;
         public Double ySpeed;
-        public Double zSpeed;
 
         public int ID;
         public int universe;
@@ -34,11 +32,9 @@ namespace Empires.Game.GameWorld
             
             this.x = Calc.genMod() * Finals.UNIVERSE_SIZE;
             this.y = Calc.genMod() * Finals.UNIVERSE_SIZE;
-            this.z = Calc.genMod() * Finals.UNIVERSE_SIZE;
 
             this.xSpeed = Calc.genMod() * Finals.UNIVERSE_SPEED;
             this.ySpeed = Calc.genMod() * Finals.UNIVERSE_SPEED;
-            this.zSpeed = Calc.genMod() * Finals.UNIVERSE_SPEED;
 
             if (generate)
             {
