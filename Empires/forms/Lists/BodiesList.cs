f722591dd8b8dcb491eca939dbe169823daa52e7
Dirk Schut <dirk@mrdls.com>
2014-09-23 13:25:29 +0200

    And now the bodies list can get the bodies in a specific unviverse

--- Empires/forms/Lists/BodiesList.cs
+++ Empires/forms/Lists/BodiesList.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,7 +30,61 @@ namespace Empires.forms
 
         public void setUniverse(int universe)
         {
-            
+            this.cb_SpecificGalaxy.Checked = false;
+            this.cb_SpecificSolarSystem.Checked = false;
+            this.cb_SpecificUniverse.Checked = true;
+            loadUniverses();
+            this.cb_Universe.SelectedIndex = universe;
+            loadData();
+        }
+
+        private void loadData()
+        {
+            this.lb_Bodies.Items.Clear();
+            foreach (Body body in Objects.game.data.bodies)
+            {
+                if (this.cb_Universe.SelectedIndex >= 0)
+                {
+                    int solarSystem = body.solarSystem;
+                    int galaxy = Objects.game.data.solarSystems[solarSystem].galaxy;
+                    int universe = Objects.game.data.galaxies[galaxy].universe;
+
+                    if (this.cb_SpecificSolarSystem.Checked)
+                    {
+                        if (solarSystem == this.cb_Universe.SelectedIndex)
+                        {
+                            this.lb_Bodies.Items.Add(body.ID + " - " + body.name);
+                        }
+                    }
+                    else if (this.cb_SpecificGalaxy.Checked)
+                    {
+                        if (galaxy == this.cb_Universe.SelectedIndex)
+                        {
+                            this.lb_Bodies.Items.Add(body.ID + " - " + body.name);
+                        }
+                    }
+                    else if (this.cb_SpecificUniverse.Checked)
+                    {
+                        if (universe == this.cb_Universe.SelectedIndex)
+                        {
+                            this.lb_Bodies.Items.Add(body.ID + " - " + body.name);
+                        }
+                    }
+                }
+                else
+                {
+                    this.lb_Bodies.Items.Add(body.ID + " - " + body.name);
+                }
+            }
+        }
+
+        private void loadUniverses()
+        {
+            this.cb_Universe.Items.Clear();
+            foreach (Universe universe in Objects.game.data.universes)
+            {
+                this.cb_Universe.Items.Add(universe.ID + " - " + universe.name);
+            }
         }
     }
 }
